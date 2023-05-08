958d0fe6cfed890c3bf84f65ef9177eb098cfa8e
kneitmann <43993305+kneitmann@users.noreply.github.com>
2023-05-08 12:55:59 +0200

    Export Import Interfaces

---
+++ src/rdf/IExport.js
@@ -0,0 +1,6 @@
+// Export der gegebenen Triple Parameter und Dateibasis ins Dateisystem
+const IExport = {
+    exportTriples: function(tripleList) {
+  
+    }
+  };
---
+++ src/rdf/IImport.js
@@ -0,0 +1,7 @@
+// Import der Daten fuer die gegebene Datei
+// Wenn replace true ist, dann die gegebene Datei ersetzten, ansonsten die Daten anhaengen
+const IImport = {
+    importFromFile: function(file, replace) {
+      
+    }
+  };

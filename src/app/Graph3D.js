7e99c867529c397952dff71524d8aea1b8036a30
kneitmann <43993305+kneitmann@users.noreply.github.com>
2023-05-11 01:01:02 +0200

    Graph3D structure without functionality

---
+++ src/app/Graph3D.js
@@ -0,0 +1,9 @@
+//import { TWEEN } from '@tweenjs/tween.js';
+//import TWEEN from '@tweenjs/tween.js';
+//import ForceGraph3D from '3d-force-graph';
+
+function graph3D() {
+    //const myGraph = ForceGraph3D();
+}
+
+export { graph3D };

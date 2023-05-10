a6113b6ad81c2b1a8729e584501748477ec8fb35
kneitmann <43993305+kneitmann@users.noreply.github.com>
2023-05-10 20:44:31 +0200

    import binary for windows

--- src/rdf/import/ImportBinaryWindows.js
+++ src/rdf/import/ImportBinaryWindows.js
@@ -1,41 +1,26 @@
-function importBinary(file, replace) {
-    // Create a new object for the file dialog
-    const dialog = require('electron').remote.dialog;
+import React, { useState } from 'react';
 
-    // Display the file selection dialog
-    dialog.showOpenDialog({
-        // Set some options for the dialog
-        buttonLabel: 'Öffnen',
-        properties: ['openFile'],
-        filters: [
-            // Filter for the file selection dialog
-            { name: 'Binärdatei', extensions: ['bin'] }
-        ]
-    }).then(result => {
-        // If the user has selected a file
-        if (!result.canceled) {
-            // Get the path to the selected file
-            const filePath = result.filePaths[0];
+function importBinary(replace) {
+    // Create a new <input> element of type "file
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
 
-            // Read the contents of the file
-            const fs = require('fs');
-            fs.readFile(filePath, 'binary', (err, content) => {
-                if (err) {
-                    console.log(`Fehler beim Lesen der Datei: ${err.message}`);
-                } else {
-                    // If replace is set, replace the contents of the file
-                    if (replace) {
-                        file.setContent(content);
-                    } else {
-                        file.appendContent(content);
-                    }
-                    console.log(`Datei erfolgreich importiert aus: ${filePath}`);
-                }
-            });
-        }
-    }).catch(err => {
-        console.log(`Fehler beim Öffnen des Dateiauswahldialogs: ${err.message}`);
+    // Add an event handler to get the selected file path.
+    fileInput.addEventListener('change', event => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            console.log(`Datei erfolgreich importiert aus: ${file.name}`);
+            console.log(`Dateiinhalt: ${file.readAsBinaryString}`)
+        };
+        reader.readAsBinaryString(file);
     });
+
+    fileInput.click();
 }
 
+function importBinaryTest() {
+    importBinary(false);
+}
 
+export { importBinaryTest };

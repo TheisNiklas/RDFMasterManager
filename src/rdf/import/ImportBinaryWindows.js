560110bdd4b225e40d6fe4019cdbcd7d78aaf424
kneitmann <43993305+kneitmann@users.noreply.github.com>
2023-05-11 02:55:04 +0200

    Import binary windows data structure prototype

--- src/rdf/import/ImportBinaryWindows.js
+++ src/rdf/import/ImportBinaryWindows.js
@@ -5,15 +5,22 @@ function importBinary(replace) {
     const fileInput = document.createElement('input');
     fileInput.type = 'file';
 
+    // Define the onload function outside the change event listener
+    const handleFileRead = function (event) {
+        const content = event.target.result;
+        const tripleList = content.split('\n').map(line => {
+            const [subject, predicate, object] = line.split(' ');
+            return { subject, predicate, object };
+        });
+        console.log(tripleList);
+    };
+
     // Add an event handler to get the selected file path.
     fileInput.addEventListener('change', event => {
         const file = event.target.files[0];
         const reader = new FileReader();
-        reader.onload = () => {
-            console.log(`Datei erfolgreich importiert aus: ${file.name}`);
-            console.log(`Dateiinhalt: ${file.readAsBinaryString}`)
-        };
-        reader.readAsBinaryString(file);
+        reader.onload = handleFileRead; // Assign the onload function here
+        reader.readAsText(file);
     });
 
     fileInput.click();

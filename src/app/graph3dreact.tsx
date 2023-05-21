831d60906cac7476316c5a13476c7d18fc78b8f0
sarah.flohr <sarah.flohr@uni-bielefeld.de>
2023-05-21 11:06:01 +0200

    add delete and re-name option to link

--- src/app/graph3dreact.tsx
+++ src/app/graph3dreact.tsx
@@ -8,6 +8,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Alert from "@mui/material/Alert";
 
 const NoSSRForceGraph = dynamic(() => import("./lib/NoSSRForceGraph"), {
   ssr: false,
@@ -15,30 +16,25 @@ const NoSSRForceGraph = dynamic(() => import("./lib/NoSSRForceGraph"), {
 
 /**
  * TODO:
- * leftClickNode: Info
- * leftClickRelation: Info
- * rightClickNode: alle umbenenne
- * rightClickRelation: individuell umbenenne, löschen
+ * Warning hinzufügen, wenn Triple-Umbenennung leer ist
+ * Delete Confirmation zu Triple-Delete hinzufügen
  */
 
 export default function Graph3DReact() {
-  const data = {
-    nodes: [{ id: "1" }, { id: "2" }, { id: "3" }],
-    links: [
-      { source: "1", target: "2" },
-      { source: "1", target: "3" },
-    ],
-  };
-
   const [openNodeLeft, setOpenNodeLeft] = React.useState(false);
   const [openLinkLeft, setOpenLinkLeft] = React.useState(false);
   const [openNodeRight, setOpenNodeRight] = React.useState(false);
+  const [openLinkRight, setOpenLinkRight] = React.useState(false);
 
   const [nodeId, setNodeId] = React.useState("");
   const [linkSource, setLinkSource] = React.useState("");
   const [linkTarget, setLinkTarget] = React.useState("");
 
   const [formField, setFormField] = React.useState("");
+
+  const [source, setSource] = React.useState("");
+  const [target, setTarget] = React.useState("");
+  const [pred, setPred] = React.useState("");
 
   const handleNodeLeftClose = () => {
     setOpenNodeLeft(false);
@@ -50,6 +46,10 @@ export default function Graph3DReact() {
 
   const handleNodeRightClose = () => {
     setOpenNodeRight(false);
+  };
+
+  const handleLinkRightClose = () => {
+    setOpenLinkRight(false);
   };
 
   const handleNodeLeftClick = (node: any) => {
@@ -68,11 +68,24 @@ export default function Graph3DReact() {
     setOpenNodeRight(true);
   };
 
-  const handleLinkRightClick = (link: any) => {};
+  const handleLinkRightClick = (link: any) => {
+    setOpenLinkRight(true);
+  };
 
-  const handleSubmit = () => {
+  const handleSubmitNodeRight = () => {
     console.log(formField);
     setOpenNodeRight(false);
+  };
+
+  const handleSubmitLinkRight = () => {
+    console.log(source);
+    console.log(target);
+    console.log(pred);
+    setOpenLinkRight(false);
+  };
+
+  const handleDeleteTriple = () => {
+    console.log("delete triple");
   };
 
   return (
@@ -111,7 +124,39 @@ export default function Graph3DReact() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleNodeRightClose}>Cancel</Button>
-          <Button onClick={handleSubmit}>Submit</Button>
+          <Button onClick={handleSubmitNodeRight}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={openLinkRight} onClose={handleLinkRightClose}>
+        <DialogTitle id="link-right-title">
+          {" "}
+          Triple Löschen oder umbenennen
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleDeleteTriple}>Delete Triple</Button>
+        </DialogActions>
+        <DialogContent>
+          <DialogContentText id="link-right-text">
+            <TextField
+              label="source"
+              variant="outlined"
+              onChange={(event) => setSource(event.target.value)}
+            />
+            <TextField
+              label="target"
+              variant="outlined"
+              onChange={(event) => setTarget(event.target.value)}
+            />
+            <TextField
+              label="pred"
+              variant="outlined"
+              onChange={(event) => setPred(event.target.value)}
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleLinkRightClose}>Cancel</Button>
+          <Button onClick={handleSubmitLinkRight}>Submit</Button>
         </DialogActions>
       </Dialog>
     </div>

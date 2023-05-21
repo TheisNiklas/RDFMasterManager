5a02f13d47abed152db464b8a4d10e14500a924b
sarah.flohr <sarah.flohr@uni-bielefeld.de>
2023-05-21 10:35:43 +0200

    add change option to node

--- src/app/graph3dreact.tsx
+++ src/app/graph3dreact.tsx
@@ -1,10 +1,13 @@
 import dynamic from "next/dynamic";
 import myData from "./testDataGraph3D";
 import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
+import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const NoSSRForceGraph = dynamic(() => import("./lib/NoSSRForceGraph"), {
   ssr: false,
@@ -29,10 +32,13 @@ export default function Graph3DReact() {
 
   const [openNodeLeft, setOpenNodeLeft] = React.useState(false);
   const [openLinkLeft, setOpenLinkLeft] = React.useState(false);
+  const [openNodeRight, setOpenNodeRight] = React.useState(false);
 
   const [nodeId, setNodeId] = React.useState("");
   const [linkSource, setLinkSource] = React.useState("");
   const [linkTarget, setLinkTarget] = React.useState("");
+
+  const [formField, setFormField] = React.useState("");
 
   const handleNodeLeftClose = () => {
     setOpenNodeLeft(false);
@@ -40,6 +46,10 @@ export default function Graph3DReact() {
 
   const handleLinkLeftClose = () => {
     setOpenLinkLeft(false);
+  };
+
+  const handleNodeRightClose = () => {
+    setOpenNodeRight(false);
   };
 
   const handleNodeLeftClick = (node: any) => {
@@ -53,9 +63,17 @@ export default function Graph3DReact() {
     setOpenLinkLeft(true);
   };
 
-  const handleNodeRightClick = (node: any) => {};
+  const handleNodeRightClick = (node: any) => {
+    setNodeId(node.id);
+    setOpenNodeRight(true);
+  };
 
   const handleLinkRightClick = (link: any) => {};
+
+  const handleSubmit = () => {
+    console.log(formField);
+    setOpenNodeRight(false);
+  };
 
   return (
     <div>
@@ -79,6 +97,22 @@ export default function Graph3DReact() {
           Source: {linkSource}
           Target: {linkTarget}
         </DialogContentText>
+      </Dialog>
+      <Dialog open={openNodeRight} onClose={handleNodeRightClose}>
+        <DialogTitle id="node-right-title">Änderungen: {nodeId}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="node-right-text">
+            <TextField
+              label="Name"
+              variant="outlined"
+              onChange={(event) => setFormField(event.target.value)}
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleNodeRightClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </DialogActions>
       </Dialog>
     </div>
   );

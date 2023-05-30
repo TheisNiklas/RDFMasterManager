21ff392190e5a485c033c1cffdfbd038624c6d01
svea <svea@worms-web.de>
2023-05-30 15:58:58 +0200

    fixed class BitVector

--- src/rdf/bitvector-tools.js
+++ src/rdf/bitvector-tools.js
@@ -18,32 +18,32 @@ export class BitvectorTools {
   }
 }
 
+
 export class BitVector{
-  constructor(size){
-    this.size = size;
+  constructor(){
+    this.bits = 0;
   }
   
   getBit(index) {
-    if (index >= this.size) {
-      throw new Error('Index out of range');
+    if (index >= this.toString().length) {
+      return 0;
     }
-
-    const mask = BigInt(1) << BigInt(index);
-    return (this.bits & mask) !== BigInt(0) ? 1 : 0;
+    const mask = 1 << index;
+    return (this.bits & mask) === 0 ? 0 : 1;
   }
 
   setBit(pos) {
-    if (pos >= this.size) {
+    if (pos >= this.toString().length) {
       throw new Error('Index out of range');
     }
-    this.bits |= BigInt(1) << BigInt(pos);
+    this.bits |= 1 << pos;
   }
 
   unsetBit(pos) {
-    if (pos >= this.size) {
+    if (pos >= this.toString().length) {
       throw new Error('Index out of range');
     }
-    this.bits &= ~(BigInt(1) << BigInt(pos));
+    this.bits &= ~(1 << pos);
   }
 
   static rank(index) {
@@ -57,17 +57,29 @@ export class BitVector{
   }
 
   static select(count) {
-    for (let index = 0; index < this.size; index++) {
+    for (let index = 0; index < this.toString().length; index++) {
       count -= this.getBit(index);
       if (count === 0) {
         return index;
       }
     }
-    return size;
+    return -1;
   }
   
    toString() {
-    const bitString = this.bits.toString(2).padStart(this.size, '0');
+    const bitString = this.bits.toString(2);
     return bitString.split('').reverse().join('');
   }
 }
+
+// let bitvector = new BitVector();
+
+// console.log(bitvector.toString());
+// console.log(bitvector.toString().length);
+// console.log(bitvector.bits);
+// bitvector.setBit(3)
+// console.log(bitvector.toString())
+// console.log(bitvector.getBit(3))
+// bitvector.unsetBit(3)
+// console.log(bitvector.toString())
+// console.log(bitvector.bits)

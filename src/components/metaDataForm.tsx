e7b03a7eda79ab98545fbcb601ed44ad6317b782
Joshua Martin <joshuamartin997@googlemail.com>
2023-06-27 21:35:33 +0200

    Add comments and refactor metaDataForm

--- src/components/metaDataForm.tsx
+++ src/components/metaDataForm.tsx
@@ -2,6 +2,7 @@
  * Contributions made by:
  * Niklas Theis
  * Bjarne Küper
+ * Kai Joshua Martin
  */
 
 import {
@@ -42,6 +43,10 @@ const SubmitButton = styled(Button)(() => ({
   width: "100%",
 }));
 
+/**
+ * Form to add and modify metadata
+ * @returns React Component MetaDataForm
+ */
 const MetaDataForm = () => {
   const [nodeColorValue, setNodeColorValue] = useState("");
   const [nodeColorValid, setNodeColorValid] = useState(true);
@@ -58,94 +63,65 @@ const MetaDataForm = () => {
 
   const dispatch = useDispatch();
 
+  /**
+   * Update link color. Check if value is valid.
+   * Only local var is updated. MetaData is not changed yet! 
+   * @param event Change event that has been triggered
+   */
   const handleChangeLinkColor = (event: ChangeEvent<HTMLInputElement>) => {
     setLinkColorValue(event.target.value);
     setLinkColorValid(colorRegex.test(event.target.value));
   };
 
+  /**
+   * Update node color. Check if value is valid.
+   * Only local var is updated. MetaData is not changed yet! 
+   * @param event Change event that has been triggered
+   */
   const handleChangeNodeColor = (event: ChangeEvent<HTMLInputElement>) => {
     setNodeColorValue(event.target.value);
     setNodeColorValid(colorRegex.test(event.target.value));
   };
 
-  const submitMetdata = () => {
-    const metaDataPossibilities = ["arrowColor", "nodeColor"];
-    for (const possibility of metaDataPossibilities) {
-      var updated = false;
-      for (const item of metaData) {
-        const predicateValue = database.dictionary.getElementById(item.predicate).replace("METADATA:", "") as string;
-        if (predicateValue == possibility) {
-          switch (predicateValue) {
-            case "arrowColor": {
-              if (linkColorValue != "") {
-                setMetadataElement(item, linkColorValue, possibility);
-              }
-              updated = true;
-              break;
-            }
-            case "nodeColor": {
-              if (nodeColorValue != "") {
-                setMetadataElement(item, nodeColorValue, possibility);
-              }
-              updated = true;
-              break;
-            }
-            default: {
-              // Code for other cases (if needed)
-              break;
-            }
-          }
-        }
-      }
-      //Triple doesnt exist in Database
-      if (!updated) {
-        //ADD TRIPLE
-        switch (possibility) {
-          case "arrowColor": {
-            if (linkColorValue != "") {
-              addMetadataElement(possibility, linkColorValue);
-            }
-            break;
-          }
-          case "nodeColor": {
-            if (nodeColorValue != "") {
-              addMetadataElement(possibility, nodeColorValue);
-            }
-            break;
-          }
-          default: {
-            // Code for other cases (if needed)
-            break;
-          }
-        }
-      }
-      let metaDataQueried = QueryCall.queryCallData(
-        [{ subject: "RDFCSA:METADATA", predicate: "", object: "" }],
-        database
-      );
-      if (metaDataQueried) {
-        dispatch(setMetaData(metaDataQueried));
-      }
-    }
-    let metaDataQueried = QueryCall.queryCallData(
-      [{ subject: "RDFCSA:METADATA", predicate: "", object: "" }],
-      database
-    );
-    if (metaDataQueried) {
-      dispatch(setMetaData(metaDataQueried));
-    }
-  };
+  /**
+   * Show a toast msg.
+   * @param msg Message that should be displayed
+   */
+  const showToast = (msg: string) => {
+    setToastMessage(msg);
+    setToastOpen(true);
+  }
 
-  const setMetadataElement = (element: any, newValue: string, possibility: string) => {
+  /**
+   * Update database. Query all triples.
+   * @param newDatabase Updated database
+   */
+  const updateCurrentData = (newDatabase) => {
+    if (newDatabase.tripleCount < 10000) {
+      // Query new data
+      const queryManager = new QueryManager(newDatabase);
+      dispatch(setCurrentData(queryManager.getTriples([new QueryTriple(null, null, null)])));
+    } else {
+      // Don't query automatically for better usability
+      showToast("Dataset exceeds 10k triples - Data not queries - query manually");
+    }
+  }
+
+  /**
+   * Update the object of a a meta data triple.
+   * @param element Meta data triple that should be updated
+   * @param possibility Predicate of meta data triple
+   * @param newValue New value that should be set as the triple object
+   */
+  const updateMetaTriple = (element: any, possibility: string, newValue: string,) => {
     const rdfOperations = new RdfOperations(database);
     const newDatabase = rdfOperations.modifyTriple(element, "RDFCSA:METADATA", "METADATA:" + possibility, newValue);
     dispatch(setDatabase(newDatabase as Rdfcsa));
 
-    setToastMessage("Dataset exceeds 10k triples - Data not queries - query manually");
-    setToastOpen(true);
+    updateCurrentData(newDatabase);
   };
 
-  const addMetadataElement = (possibility: string, value: string) => {
+  const insertMetaTriple = (possibility: string, value: string) => {
     const rdfOperations = new RdfOperations(database);
     const newDatabase = rdfOperations.addTriple(
       "RDFCSA:METADATA",
@@ -154,10 +130,89 @@ const MetaDataForm = () => {
     );
     dispatch(setDatabase(newDatabase));
 
-    setToastMessage("Dataset exceeds 10k triples - Data not queries - query manually");
-    setToastOpen(true);
+    updateCurrentData(newDatabase);
   };
 
+  /**
+   * Update or insert a meta data Triple.
+   * @param possibility Predicate of Meta Triple that should be updated or inserted.
+   * @param metaTriple Meta Triple that will be updated. If null a new triple will be inserted.
+   */
+  const setMetaTriple = (possibility: string, metaTriple: any,) => {
+    let value = "";
+    switch (possibility) {
+      case "arrowColor": {
+        if (linkColorValue !== "") {
+          value = linkColorValue;
+        }
+        break;
+      }
+      case "nodeColor": {
+        if (nodeColorValue !== "") {
+          value = nodeColorValue;
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    // Early exit
+    if (value === "") {
+      return
+    }
+
+    if (metaTriple !== null) {
+      // Update
+      updateMetaTriple(metaTriple, possibility, value);
+    } else {
+      // Insert
+      insertMetaTriple(possibility, value);
+    }
+  };
+
+
+  /**
+   * Update current meta data. Query for meta data and set it into state.
+   */
+  const updateMetaData = () => {
+    let metaDataQueried = QueryCall.queryCallData(
+      [{ subject: "RDFCSA:METADATA", predicate: "", object: "" }],
+      database
+    );
+    if (metaDataQueried) {
+      dispatch(setMetaData(metaDataQueried));
+    }
+  }
+
+  /**
+   * Update metadata on submit. Update existing nodes or insert new node if meta data node doesnt exists yet.
+   */
+  const submitMetdata = () => {
+    const metaDataPossibilities = ["arrowColor", "nodeColor"];
+    // Loop over all meta data possibilities
+    for (const possibility of metaDataPossibilities) {
+      var triple_exists = false;
+      // Loop over existing meta data triples and check if they match current possibility.
+      for (const item of metaData) {
+        const predicateValue = database.dictionary.getElementById(item.predicate).replace("METADATA:", "") as string;
+
+        if (predicateValue === possibility) {
+          triple_exists = true;
+          setMetaTriple(possibility, item);
+        }
+      }
+
+      //Triple doesn't exist in Database. We need to insert a new triple.
+      if (!triple_exists) {
+        setMetaTriple(possibility, null);
+      }
+
+      updateMetaData()
+    }
+  };
+
+  // Close Popup
   const handleToastClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === "clickaway") {
       return;
